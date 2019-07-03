f3c77f2c8fb455d842bcf21636cf40c0d216ea45
Taylor <stein0209@gmail.com>
2019-07-03 12:41:06 -0600

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_pigeon = {}
+  data.each do |key, value|
+    value.each do |attribute, p_names|
+      p_names.each do |p_name|
+        if !new_pigeon[p_name]
+          new_pigeon[p_name] = {}
+        end
+
+        if !new_pigeon[p_name][key]
+          new_pigeon[p_name][key] = []
+        end
+
+        new_pigeon[p_name][key] << attribute.to_s
+        
+      end
+    end
+  end
+  new_pigeon
 end
