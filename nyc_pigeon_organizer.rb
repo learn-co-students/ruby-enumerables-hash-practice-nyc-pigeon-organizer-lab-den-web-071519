de2cf3f4810de7b10da0debf17ba2647dbd3ad98
Ethan Collins <ethantcollins98@gmail.com>
2019-07-07 16:13:56 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  newHash = {}
+  namesIsolated =  []
+
+  #names = data[:color].values
+  data[:color].map {|colors, names|names.map {|name|namesIsolated.push(name) unless namesIsolated.include? name}} #adds array of unique names to namesIsolated
+
+  puts namesIsolated
+  namesIsolated.each do |name|
+    newHash[name] = {color: [], gender: [], lives: []}
+  end
+
+  data[:color].map {|colors, names| names.map {|name| newHash[name][:color].push(colors.to_s)}}
+  data[:gender].map {|gender, names| names.map {|name| newHash[name][:gender].push(gender.to_s)}}
+  data[:lives].map{|lives, names| names.map {|name| newHash[name][:lives].push(lives.to_s)}}
+  newHash
+
 end
