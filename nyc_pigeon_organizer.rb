65a4b592676978b5e8169644a26d1b21fce7b591
Tyler Masterson <tylercmasterson@gmail.com>
2019-07-08 18:30:14 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
+require "pry"
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  list = {}
+  data.each do |k, v|
+     v.each do |k2, names|
+       names.each do |name|
+       if !list[name]
+         list[name] = {}
+       end
+       if !list[name][k]
+         list[name][k] = []
+       end
+       list[name][k] << k2.to_s
+      end
+    end
+  end
+  list
 end
+
+#take current hash and extract names values
+#loop through each key/value pair in each hash to find if values match names
+#if there is a match, 
